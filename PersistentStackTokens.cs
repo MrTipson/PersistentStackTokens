8e05922018908e7bb25acad896eabae4b434f2f3
MrTipson <gunfight7.gf@gmail.com>
2021-04-12 18:39:55 +0200

    Add project files.

---
+++ PersistentStackTokens.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using RoR2;
+using UnityEngine.Networking;
+namespace MrTipson
+{
+    [BepInPlugin("com.MrTipson.PersistentStackTokens", "Keep Bandit stack tokens between stages", "1.0.0")]
+    public class PersistentStackTokens : BaseUnityPlugin
+    {
+        // Keep track of the stack count for all players in the lobby (as the host)
+        private int[] stackCount;
+        private double multiplier;
+        private string config = "config.txt";
+        public void Awake()
+        {
+            // Save stack counts as stage ends
+            SceneExitController.onBeginExit += (SceneExitController exitController) =>
+            {
+                // We shouldnt try to do anything if we arent the host
+                if (!NetworkServer.active)
+                {
+                    return;
+                }
+
+                for(int i = 0; i < PlayerCharacterMasterController.instances.Count; i++)
+                {
+                    CharacterBody body = PlayerCharacterMasterController.instances[i].master.GetBody();
+                    if (body)
+                    {
+                        stackCount[i] = (int)(body.GetBuffCount(RoR2Content.Buffs.BanditSkull) * multiplier);
+                        Logger.LogDebug(stackCount[i]);
+                    }
+                }
+            };
+            // Apply saved counts as stage begins
+            Stage.onStageStartGlobal += (Stage stage) =>
+            {
+                // We shouldnt try to do anything if we arent the host
+                if (!NetworkServer.active)
+                {
+                    return;
+                }
+
+                for(int i = 0; i < PlayerCharacterMasterController.instances.Count; i++)
+                {
+                    CharacterBody body = PlayerCharacterMasterController.instances[i].master.GetBody();
+                    if (body)
+                    {
+                        for (int j = 0; j < stackCount[i]; j++)
+                        {
+                            body.AddBuff(RoR2Content.Buffs.BanditSkull);
+                        }
+                    }
+                }
+            };
+            // Reset stack count when run starts
+            Run.onRunStartGlobal += (Run run) =>
+            {
+                // We shouldnt try to do anything if we arent the host
+                if (!NetworkServer.active)
+                {
+                    return;
+                }
+
+                stackCount = new int[PlayerCharacterMasterController.instances.Count];
+            };
+
+            if (!(System.IO.File.Exists(config) && double.TryParse(System.IO.File.ReadAllText(config), out multiplier)))
+            {
+                System.IO.File.WriteAllText(config, "1");
+                multiplier = 1;
+            }
+            Logger.LogDebug(multiplier);
+        }
+    }
+}
